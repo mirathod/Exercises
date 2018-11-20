3349bbf32f6721c85ff941787a8daf6bc0c9a385
mirathod <milan.rathod0510@gmail.com>
2018-11-21 02:34:51 +0530

    nov-20-problems

---
+++ src/test/java/org/milan/hackerearth/BatmanAndTickTackToeTest.java
@@ -0,0 +1,28 @@
+package org.milan.hackerearth;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test Class for {@link BatmanAndTickTackToe}
+ *
+ * @author Milan Rathod
+ */
+public class BatmanAndTickTackToeTest {
+
+    @Test
+    public void isWonScenarioAvailable() {
+        char[][] matrix = {
+                {'x', '.', '.', 'x'},
+                {'.', 'o', 'x', '.'},
+                {'o', '.', '.', '.'},
+                {'o', 'x', 'o', '.'}
+        };
+
+        BatmanAndTickTackToe batmanAndTickTackToe = new BatmanAndTickTackToe();
+
+        boolean result = batmanAndTickTackToe.isWonScenarioAvailable(matrix);
+
+        Assert.assertTrue(result);
+    }
+}
